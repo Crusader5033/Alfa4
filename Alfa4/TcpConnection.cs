d9e03fb13f4ab922b3aa399e1863fbec8cfe9908
Dominik Prochazka <113086006+Crusader5033@users.noreply.github.com>
2024-02-25 15:50:28 +0100

    Added Tcp v1 , for testing, no message history

---
+++ Alfa4/TcpConnection.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+internal class TcpConnection
+{
+    private TcpClient tcpClient;
+    private NetworkStream stream;
+    private string ipAddress;
+
+    public TcpConnection(string ipAddress)
+    {
+        this.ipAddress = ipAddress;
+        tcpClient = new TcpClient();
+    }
+
+    public void Connect()
+    {
+        try
+        {
+            tcpClient.Connect(ipAddress, 9876);
+            stream = tcpClient.GetStream();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occurred while connecting over TCP: " + ex.Message);
+        }
+    }
+
+    public void SendMessage(string message)
+    {
+        try
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine($"Sent TCP message: {message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occurred while sending TCP message: " + ex.Message);
+        }
+    }
+
+    public void Disconnect()
+    {
+        try
+        {
+            stream.Close();
+            tcpClient.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occurred while disconnecting TCP connection: " + ex.Message);
+        }
+    }
+}
--- Alfa4/UDP/Peer.cs
+++ Alfa4/UDP/Peer.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using Alfa4;
+using System.Net.Sockets;
 using System.Net;
 using System.Text;
 
@@ -7,18 +8,20 @@ internal class Peer
     private string peerId;
     private int discoveryPort;
     private int discoveryIntervalSeconds;
-    private  UdpClient udpClient;
-    private  UdpListener udpListener;
+    private readonly UdpClient udpClient;
+    private readonly UdpListener udpListener;
+    private readonly Dictionary<string, TcpConnection> tcpConnections;
 
     public Peer(string id, int port, int intervalSeconds)
     {
-        this.peerId = id;
-        this.discoveryPort = port;
-        this.discoveryIntervalSeconds = intervalSeconds;
-        this.udpClient = new UdpClient();
+        peerId = id;
+        discoveryPort = port;
+        discoveryIntervalSeconds = intervalSeconds;
+        udpClient = new UdpClient();
         udpClient.EnableBroadcast = true;
         udpListener = new UdpListener(discoveryPort);
         udpListener.MessageReceived += HandleDiscoveryResponse;
+        tcpConnections = new Dictionary<string, TcpConnection>();
     }
 
     public void StartDiscovery()
@@ -34,7 +37,7 @@ internal class Peer
             while (true)
             {
                 SendDiscoveryRequest();
-                Thread.Sleep(this.discoveryIntervalSeconds * 1000);
+                Thread.Sleep(discoveryIntervalSeconds * 1000);
             }
         }
         catch (Exception ex)
@@ -49,7 +52,7 @@ internal class Peer
         {
             string message = $"{{\"command\":\"hello\",\"peer_id\":\"{peerId}\"}}";
             byte[] data = Encoding.UTF8.GetBytes(message);
-            this.udpClient.Send(data, data.Length, new IPEndPoint(IPAddress.Broadcast, this.discoveryPort));
+            udpClient.Send(data, data.Length, new IPEndPoint(IPAddress.Broadcast, discoveryPort));
         }
         catch (Exception ex)
         {
@@ -60,15 +63,60 @@ internal class Peer
     private void HandleDiscoveryResponse(string message, IPEndPoint remoteEP)
     {
         Console.WriteLine($"Received response from {remoteEP}: {message}");
+
+        // Parse the response message
+        var response = ParseJsonMessage(message);
+        if (response.ContainsKey("messages"))
+        {
+            // Extract messages from response
+            var messages = response["messages"] as Dictionary<string, object>;
+            // Handle handshake
+            HandleHandshake(messages, remoteEP);
+        }
+    }
+
+    private void HandleHandshake(Dictionary<string, object> messages, IPEndPoint remoteEP)
+    {
+        if (!tcpConnections.ContainsKey(remoteEP.Address.ToString()))
+        {
+            // Connect to the peer over TCP
+            TcpConnection tcpConnection = new TcpConnection(remoteEP.Address.ToString());
+            tcpConnection.Connect();
+            tcpConnections.Add(remoteEP.Address.ToString(), tcpConnection);
+
+            // Send handshake message
+            tcpConnection.SendMessage($"{{\"command\":\"hello\",\"peer_id\":\"{peerId}\"}}");
+
+            // Send peer's chat history
+            tcpConnection.SendMessage(GetMessageHistory());
+        }
+    }
+
+    private string GetMessageHistory()
+    {
+        // Here you would construct your chat history message
+        // For now, let's just return a dummy message
+        return "{\"status\":\"ok\",\"messages\":{\"1707243010934\":{\"peer_id\":\"molic-peer3\",\"message\":\"pokus\"}}}";
+    }
+
+    private Dictionary<string, object> ParseJsonMessage(string message)
+    {
+        // Here you would parse the JSON message into a dictionary
+        // For now, let's just return an empty dictionary
+        return new Dictionary<string, object>();
     }
 
     public void StartListening()
     {
-        this.udpListener.StartListening();
+        udpListener.StartListening();
     }
 
     public void Stop()
     {
-        this.udpListener.Close();
+        foreach (var connection in tcpConnections.Values)
+        {
+            connection.Disconnect();
+        }
+        udpListener.Close();
     }
 }
